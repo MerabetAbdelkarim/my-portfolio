b74ffae4485898941e04542908df0d2ee53af27a
MerabetAbdelkarim <merabet.abdelkarim.dev@gmail.com>
2023-12-16 15:20:52 +0100

    update social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,10 +14,10 @@ export const Footer = () => {
                     </Col>
                     <Col size={12} sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/merabet-abdelkarim/"><FaLinkedinIn /></a>
-                            <a href="https://www.facebook.com/karimkarimmerabet/"><FaFacebook /></a>
-                            <a href="https://www.behance.net/abdelkamerabet"><FaBehance /></a>
-                            <a href="https://www.youtube.com/@merabetabdelkarim"><FaYoutube /></a>
+                            <a href="https://www.linkedin.com/in/merabet-abdelkarim/" target="_blank"><FaLinkedinIn /></a>
+                            <a href="https://www.facebook.com/karimkarimmerabet/" target="_blank"><FaFacebook /></a>
+                            <a href="https://www.behance.net/abdelkamerabet" target="_blank"><FaBehance /></a>
+                            <a href="https://www.youtube.com/@merabetabdelkarim" target="_blank"><FaYoutube /></a>
                         </div>
                         <p>Copyright 2023. All Rights Reserved</p>
                     </Col>
